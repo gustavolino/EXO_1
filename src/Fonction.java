0e080557db927f147c69ac26f44570a2b5df1119
DEGLASSE <guillaumedeglasse@hotmail.fr>
2015-10-16 14:52:46 +0200

    ajout fonction bizzbuzz

--- src/Fonction.java
+++ src/Fonction.java
@@ -3,13 +3,13 @@ public class Fonction {
 	
 	public static void main(String[] args) {
 		
-		bizz(8);
+		bizzbuzz(8,15);
+		
 			
 	}
-	public static void bizz (int n){
+	public static void bizzbuzz (int n, int m){
 		
-		System.out.println(n);
+		System.out.println(n+m);
 	}
-	
 	
 }
