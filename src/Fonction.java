cfdbe9e2f8211ad083907687e5791ccf45678be7
DEGLASSE <guillaumedeglasse@hotmail.fr>
2015-10-16 15:02:48 +0200

    ajout fonction buzz

--- src/Fonction.java
+++ src/Fonction.java
@@ -3,11 +3,11 @@ public class Fonction {
 	
 	public static void main(String[] args) {
 		
-		bizzbuzz(8,15);
+		buzz(8,15);
 		
 			
 	}
-	public static void bizzbuzz (int n, int m){
+	public static void buzz (int n, int m){
 		
 		System.out.println(n+m);
 	}
